f85e88f0bd9a40087781843c4a2bb77eccd08beb
Niels <nielsz@pm.me>
2023-12-27 13:42:40 +0100

    Update kiosk_driver and add socket.io functionality

--- run.py
+++ run.py
@@ -2,23 +2,30 @@ import time
 from serve2 import DingesServer
 from chromium import kiosk_driver, kiosk_subprocess
 
-menu = DingesServer('menu', 8000)
+print('Launching chrome...')
+kiosk = kiosk_driver()
+print('Launching menu...')
+
+
+menu = DingesServer('menu', 8000, socketio=True)
+@menu.socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+    kiosk.get('chrome://gpu/')
+
 menu.start()
+kiosk.get(menu.url)
 
 gunwizard = DingesServer('games/gunwizard', 8005)
 gunwizard.start()
 
-
 # pkiosk = kiosk_subprocess(menu.url, gunwizard.url)
-kiosk = kiosk_driver(menu.url)
-time.sleep(5)
-kiosk.get('chrome://gpu/')
-time.sleep(10)
+# kiosk.get('chrome://gpu/')
 
 # while pkiosk.poll() is None:
 #     print("Process is running.")
 #     time.sleep(1)
 
 
-gunwizard.stop()
-menu.stop()
+# gunwizard.stop()
+# menu.stop()
