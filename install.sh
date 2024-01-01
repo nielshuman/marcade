290e431c2b726f6608e885c2632c4804b5f03483
niels <nielsz@pm.me>
2024-01-01 17:48:12 +0000

    Remove unused code and update kiosk subprocess command

---
+++ install.sh
@@ -0,0 +1,4 @@
+git pull
+pip install flask_socketio --break-system-packages
+pip install sounddevice --break-system-packages
+sudo apt install python3-flask python3-eventlet python3-selenium python3-waitress python3-soundfile
--- update.sh
+++
@@ -1,4 +0,0 @@
-echo "Updating the project..."
-git pull
-python3 -m pip install -r requirements.txt
-echo "Update complete."
