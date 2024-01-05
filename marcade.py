0ed28eca6b1a916c8ea6078993213b29044f0804
niels <nielsz@pm.me>
2024-01-05 14:43:44 +0000

    Remove unnecessary requirements and update serve2.py and util.py

--- marcade.py
+++ marcade.py
@@ -1,18 +1,18 @@
 import os
-import time
-
 # Change the working directory to the directory of the script
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+import time
 from serve2 import DingesServer
 from kiosk import kiosk_driver, kiosk_subprocess, is_open
-from util import play_sound
+from util import play_sound, AntimicroX
 try:
     import gpiozero
 except:
     gpiozero = None
 
-
+antimciroX = AntimicroX('empty')
+antimciroX.start()
 gamesServer = DingesServer('games', 8200)
 gamesServer.start()
 
@@ -25,14 +25,11 @@ def launch_game(game):
     print('Launching game', game)
     kiosk.get(gamesServer.url + game)
 
-menuServer.start()
-kiosk.get(menuServer.url + 'coin.html')
-play_sound('audio/start.wav')
-
 def coin_inserted():
     print('Coin inserted')
     play_sound('audio/coin.mp3')
     kiosk.get(menuServer.url + 'select.html')
+    antimciroX.change_profile('enter')
 
 if gpiozero:
     coinListener = gpiozero.Button(21)
@@ -41,9 +38,14 @@ if gpiozero:
 else:
     print('Not listening for coin')
 
+play_sound('audio/start.wav')
+menuServer.start()
+kiosk.get(menuServer.url + 'coin.html')
+
 while is_open(kiosk):
     time.sleep(1)
 
-print('Browser is closed')
+print('Browser is closed, stopping servers and AntimicroX')
 gamesServer.stop()
 menuServer.stop()
+antimciroX.stop()
