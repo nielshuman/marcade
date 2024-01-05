de52031e656ae3718cf50fa6534211057d9802c8
niels <nielsz@pm.me>
2024-01-05 13:42:15 +0000

    Remove marcade.desktop and commented out code, update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+cd /home/niels/marcade
 git pull
 pip install flask_socketio sounddevice --break-system-packages
 sudo apt install python3-flask python3-eventlet python3-selenium python3-waitress python3-soundfile chromium-browser chromium-chromedriver libportaudio2 -y
-cp -f marcade.desktop ~/.config/autostart/
+cp -f shit/marcade.desktop ~/.config/autostart/
 echo "Copied marcade.desktop to ~/.config/autostart/"
+sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
