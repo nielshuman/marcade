ed525111c8eae8e5d137fb03abc9627bd5cd7d46
Niels Huisman <nielsz@pm.me>
2024-01-07 23:07:59 +0100

    Update install.sh and marcade.desktop files

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-cd /home/niels/marcade
+cd ~/marcade
 git pull
+
 pip install flask_socketio sounddevice --break-system-packages
 sudo apt install python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress python3-soundfile chromium-browser chromium-chromedriver libportaudio2 -y
-cp -f shit/marcade.desktop ~/.config/autostart/
-echo "Copied marcade.desktop to ~/.config/autostart/"
-sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
 
-# sudo cp -f shit/wait.png /usr/share/plymouth/themes/pix/splash.png
-# sudo plymouth-set-default-theme --rebuild-initrd pix
+mkdir -p ~/.config/autostart
+cp -f shit/marcade.desktop ~/.config/autostart/
+cp -f shit/marcade.desktop ~/.local/share/applications/
+cp -f shit/update.desktop ~/.local/share/applications/
+sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
+sudo apt install -f -y
+
+# only do this is files are not identical
+if ! cmp -s shit/wait.png /usr/share/plymouth/themes/pix/splash.png; then
+    echo "Applying splash screen"
+    sudo cp -f shit/wait.png /usr/share/plymouth/themes/pix/splash.png
+    sudo plymouth-set-default-theme --rebuild-initrd pix
+fi
