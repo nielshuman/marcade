05059b7d856b27ad60036ffbc9afb3d361ab6691
nielshuman <nielsz@pm.me>
2024-02-13 17:20:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 cd ~/marcade
+sudo apt isntall git git-lfs -y
 git pull
 
 pip install flask_socketio sounddevice --break-system-packages
-sudo apt install python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress python3-soundfile python3-pygame chromium-browser chromium-chromedriver libportaudio2 git-lfs -y
+sudo apt install python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress python3-soundfile python3-pygame chromium-browser chromium-chromedriver libportaudio2 -y
 
 mkdir -p ~/.config/autostart
 cp -f shit/marcade.desktop ~/.config/autostart/
