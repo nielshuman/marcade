a05d4eedeff671d78e2dac4eb8b56480081a0688
nielshuman <nielsz@pm.me>
2024-02-13 16:22:24 +0100

    Add de Krosse Reloaded

---
+++ games/reloaded/deKrosse_Reloaded_XI.pck
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f45443ee6ad8b5dd54757676a3915722b198974aa84b379c1a5abf63be27fe79
+size 156654176
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cd ~/marcade
 git pull
 
 pip install flask_socketio sounddevice --break-system-packages
-sudo apt install python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress python3-soundfile python3-pygame chromium-browser chromium-chromedriver libportaudio2 -y
+sudo apt install python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress python3-soundfile python3-pygame chromium-browser chromium-chromedriver libportaudio2 git-lfs -y
 
 mkdir -p ~/.config/autostart
 cp -f shit/marcade.desktop ~/.config/autostart/
