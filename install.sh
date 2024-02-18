36703c39d213535305eda9bc111952f76660a18f
Niels Huisman <nielsz@pm.me>
2024-02-18 20:20:52 +0100

    Update install.sh  and symlink with presets dir

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
-cd ~/marcade
 sudo apt update
-sudo apt install git git-lfs -y
+pip install flask_socketio PyOpenAL --break-system-packages
+sudo apt install git git-lfs python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress chromium-browser chromium-chromedriver libportaudio2 -y
+
+cd ~/marcade
 git pull
 git lfs pull
 
-pip install flask_socketio PyOpenAL --break-system-packages
-sudo apt install python3-flask python3-eventlet python3-selenium python3-yaml python3-waitress chromium-browser chromium-chromedriver libportaudio2 -y
-
+# sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
+sudo dpkg -i -EG shit/input-remapper-2.0.1.deb
+sudo apt install -f -y
 mkdir -p ~/.config/autostart
 cp -f shit/marcade.desktop ~/.config/autostart/
 cp -f shit/marcade.desktop ~/.local/share/applications/
 cp -f shit/update.desktop ~/.local/share/applications/
 cp -f shit/home.desktop ~/.local/share/applications/
+# only if ~/.config/input-remapper-2 does not already exist
 
-# sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
-sudo dpkg -i -EG shit/input-remapper-2.0.1.deb
-sudo apt install -f -y
+if [ ! -L ~/.config/input-remapper-2 ]; then
+    ln -s ~/marcade/inputremapper-config/ ~/.config/input-remapper-2
+fi
+
 chmod +x home.sh
-
-ln -s ./inputremapper-config/ ~/.config/input-remapper-2
 
 # only do this is files are not identical
 if ! cmp -s shit/wait.png /usr/share/plymouth/themes/pix/splash.png; then
