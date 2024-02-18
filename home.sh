a8756f6e225c645a76b0190cfef0cd741984ca2e
Niels Huisman <nielsz@pm.me>
2024-02-18 13:17:56 +0100

    Update file paths and add input-remapper package installation

--- home.sh
+++ home.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kill -s USR1 $(cat ~/marcade/.marcade.pid)
+kill -s USR1 $(cat /home/niels/marcade/.marcade.pid)
--- install.sh
+++ install.sh
@@ -13,9 +13,11 @@ cp -f shit/marcade.desktop ~/.local/share/applications/
 cp -f shit/update.desktop ~/.local/share/applications/
 cp -f shit/home.desktop ~/.local/share/applications/
 
-sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
+# sudo dpkg -i -EG shit/antimicrox-3.3.4-aarch64.deb
+sudo dpkg -i -EG shit/input-remapper-2.0.1.deb
 sudo apt install -f -y
 chmod +x home.sh
+
 # only do this is files are not identical
 if ! cmp -s shit/wait.png /usr/share/plymouth/themes/pix/splash.png; then
     echo "Applying splash screen"
