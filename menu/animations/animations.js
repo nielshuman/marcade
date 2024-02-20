f5f169bc813edb570bf95f21b6072a9957bed2dc
nielshuman <nielsz@pm.me>
2024-02-20 15:09:54 +0100

    Remove Pacman game files and update animations in menu pages

---
+++ menu/animations/animations.js
@@ -0,0 +1,27 @@
+const PACMAN_DURATION = 14000;
+const MARIO_DURATION = 5000;
+
+function animatePacman() {
+    path = document.getElementById('pacman');
+    path.classList.add('final__path');
+    path.innerHTML = `
+    <div class="block"></div>
+    <div class="final__pacman"></div>
+    <div class="final__ghost">
+      <div class="final__eyes"></div>
+      <div class="final__skirt"></div>
+    </div>
+    `
+    setTimeout(() => {
+        path.classList.remove('final__path');
+        path.innerHTML = '';
+    }, PACMAN_DURATION);
+}
+
+const mario = document.getElementById('mario');
+function animateMario() {
+    mario.classList.add('animate-mario');
+    setTimeout(() => {
+        mario.classList.remove('animate-mario');
+    }, MARIO_DURATION);
+}
